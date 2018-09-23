ed2701899e9065afd4cb38178fc084d7933ee805
Catherine Bronzo <cbronzo@mac.com>
2018-09-23 18:33:43 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,11 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
+      if @post.update(post_params)
+        redirect_to post_path(@post)
+      else
+        render :edit
+      end
       if @post.update(post_params).valid?
         @post.update(post_params)
         redirect_to post_path(@post)
